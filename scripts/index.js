ab7753917af4b1e563111cc46b907c100f8ddf83
Karen Tano <thetanos24@gmail.com>
2025-09-21 19:06:04 -0700

    delete button jss update

--- scripts/index.js
+++ scripts/index.js
@@ -80,6 +80,11 @@ function getCardElement(data) {
     cardLikeBtnEl.classList.toggle("card__like-btn_active");
   });
 
+  const cardDeleteBtnEl = cardElement.querySelector(".card__delete-btn");
+  cardDeleteBtnEl.addEventListener("click", () => {
+    cardDeleteBtnEl.closest(".card").remove();
+  });
+
   return cardElement;
 }
 
